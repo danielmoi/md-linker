609164744e8bf7dd050f8ae91db0a0a250a61fc8
danielmoi <dmoixy@gmail.com>
2021-11-20 23:54:11 +1100

    Generate link and copy to clipboard

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 import articleTitle from 'article-title';
+import axios from 'axios';
+import prepend from 'prepend-http';
+import clipboard from 'clipboardy';
 
-const args = process.env.args;
-console.log('args:', args);
+const args = process.argv;
+
+const url = args[2];
+
+const result = await axios.get(prepend(url)).catch(e => {
+  console.log('Error fetching url:', e);
+});
+
+const html = result.data;
+const title = articleTitle(html);
+console.log('title:', title);
+
+const md = `[${title}](${url})`;
+console.log('md:', md);
+
+clipboard.writeSync(md);
